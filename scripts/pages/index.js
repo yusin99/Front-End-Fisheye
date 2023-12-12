5852ca8626919ae3f6fb53bd58bd57f8c4fcd7be
Yusin <sunkatabeckama@gmail.com>
2023-12-12 12:49:15 +0100

    Adjust indentation and managing the state of the spinner

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,36 +1,46 @@
-    async function getPhotographers() {
+async function getPhotographers() {
+    const spinner = document.getElementById("spinner");
+    try {
+        spinner.style.display = "block"; // Show the spinner
 
-            try {
-                // Utilize Fetch to get data from your JSON file
-                const response = await fetch('data/photographers.json');
-                const photographers = await response.json();
-        
-                // Log the fetched data to the console for verification
-                console.log(photographers);
-        
-                // Return the photographers array
-                return { photographers };
-            } catch (error) {
-                console.error('Error fetching photographers data:', error);
-                throw error;
-            }
+        const response = await fetch("data/photographers.json");
+        const photographers = await response.json();
+
+        // Hide the spinner once the data is loaded
+        spinner.style.display = "none";
+
+        return { photographers };
+    } catch (error) {
+        console.error("Error fetching photographers data:", error);
+        throw error;
     }
+}
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(
+        ".photographer_section"
+    );
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerTemplate(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+}
 
-    async function init() {
-        // Récupère les datas des photographes
+async function init() {
+    try {
         const { photographers } = await getPhotographers();
+
+        // Display data using your actual displayData function
         displayData(photographers.photographers);
+    } catch (error) {
+        console.error("Initialization error:", error);
+    } finally {
+        // Hide the spinner in case of success or error
+        const spinner = document.getElementById("spinner");
+        spinner.style.display = "none";
     }
-    
-    init();
-    
+}
+
+init();
