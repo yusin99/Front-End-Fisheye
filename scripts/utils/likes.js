f9ae06882011ddbd105ade591140dcd826634088
Yusin <sunkatabeckama@gmail.com>
2023-12-20 20:26:01 +0100

    Update likes functionality - merging the logic into single function

--- scripts/utils/likes.js
+++ scripts/utils/likes.js
@@ -1,13 +1,22 @@
-function updateTotalLikes(totalLikes) {
-    const totalLikesElement = document.querySelector(`.total-likes`);
-    if (totalLikesElement) {
-        totalLikesElement.innerHTML = `<i class="fa fa-heart"></i> ${totalLikes}`;
+function updateLikes(item, totalLikes){
+    if (!item.liked) {
+        totalLikes++;
+        item.likes++;
+        item.liked = true;
+    } else {
+        totalLikes--;
+        item.likes--;
+        item.liked = false;
     }
-}
-
-function updateMediaLikes(item) {
     const likesElement = document.querySelector(`.likes[data-media="${item.title}"]`);
     if (likesElement) {
         likesElement.innerHTML = `${item.likes} <i class="fa ${item.liked ? 'fa-heart' : 'fa-heart-o'}"></i>`;
     }
+
+    const totalLikesElement = document.querySelector(`.total-likes`);
+    if (totalLikesElement) {
+        totalLikesElement.innerHTML = `<i class="fa fa-heart"></i> ${totalLikes}`;
+    }
+
+    return totalLikes;
 }
